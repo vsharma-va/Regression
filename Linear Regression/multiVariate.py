2e3220e49fd3a7755c81b2aad01e4f77d790b58f
Vaibhav Sharma <yavsvaibhav@gmail.com>
2021-04-27 20:37:15 +0530

    Added Predict Function

--- Linear Regression/multiVariate.py
+++ Linear Regression/multiVariate.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("../Resources/multivariate.csv", header=None)
 
@@ -12,12 +13,10 @@ m = len(X2)
 
 
 def normalizeFeatures(x):
-    firstFeature = x[:, 0]
-    secondFeature = x[:, 1]
-    firstFeature = (firstFeature - firstFeature.mean()) / firstFeature.std()
-    secondFeature = (secondFeature - secondFeature.mean()) / secondFeature.std()
-    x = np.append(firstFeature.reshape(m, 1), secondFeature.reshape(m, 1), axis=1)
-    return x
+    mean = np.mean(x, axis=0)  # in numpy axis=0 is along the columns
+    std = np.std(x, axis=0)
+    X_norm = (x - mean) / std
+    return X_norm
 
 
 X2 = normalizeFeatures(X2)
@@ -48,7 +47,7 @@ cost = computeCost(X2, Y, thetas)
 # learning rate and iterations found by trial and error
 # ideal value of learning rate is less than 1 and greater than 10^-6
 thetas, costHistory = gradientDescent(X2, Y, thetas, 0.01, 1000)
-equation = f'{thetas[0, :][0]} + {thetas[1, :][0]} * x1 + {thetas[2, :][0]} * x2'
+equation = f'{thetas[2, :][0]} + {thetas[1, :][0]} * x1 + {thetas[0, :][0]} * x2'
 print(equation)
 yCap = np.dot(X2, thetas)
 
@@ -59,3 +58,22 @@ d1 = actual - predicted
 d2 = actual - actual.mean()
 rSquared = 1 - d1.dot(d1)/d2.dot(d2)
 print('rSquared is:', rSquared)
+
+# graph of cost function
+# the cost Function decreases with correct learning rate
+# if the learning rate is very high then it will miss the global minimum and keep increasing to infinity
+# that is why we get the error invalid value when subtracting thetas and descent
+plt.plot(costHistory)
+plt.show()
+
+
+# prediction
+def prediction(size, numberBedrooms):
+    featureArray = np.array([size, numberBedrooms, 1]).reshape(1, 3)
+    costOfHouse = np.dot(featureArray, thetas)
+    return costOfHouse[0][0]
+
+
+normSampleData = normalizeFeatures(np.array([1650, 3]))
+print(prediction(normSampleData[0], normSampleData[1]))
+
